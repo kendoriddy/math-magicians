d242a83d7f49da18f8ebc8a99b9aa4ae90c4da3a
Kehinde Onifade <onifkay@gmail.com>
2022-09-13 21:18:42 +0100

    Convert App.js to functional component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Calculator from "./components/Calculator";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Calculator />
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+const App = () => {
+  <div className="App">
+    <BrowserRouter>
+      <Calculator />
+    </BrowserRouter>
+  </div>;
+};
 export default App;
