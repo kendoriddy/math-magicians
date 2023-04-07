7b809f812f3d4d09fe86d3087bdc5e3df4cadd4b
Kehinde Onifade <onifkay@gmail.com>
2023-04-07 09:03:51 +0100

    create qrcode component

---
+++ src/components/QrCode.js
@@ -0,0 +1,48 @@
+// src/components/QrCode.js
+
+import { useState } from "react";
+import { QRCodeCanvas } from "qrcode.react";
+
+const QrCode = () => {
+  const [url, setUrl] = useState("");
+
+  const downloadQRCode = (e) => {
+    e.preventDefault();
+    setUrl("");
+  };
+
+  const qrCodeEncoder = (e) => {
+    setUrl(e.target.value);
+  };
+
+  const qrcode = (
+    <QRCodeCanvas id="qrCode" value={url} size={300} bgColor={"#00ff00"} level={"H"} />
+  );
+  return (
+    <div className="qrcode__container">
+            <div>{qrcode}</div>     {" "}
+      <div className="input__group">
+               {" "}
+        <form onSubmit={downloadQRCode}>
+                    <label>Enter URL</label>
+                   {" "}
+          <input
+            type="text"
+            value={url}
+            onChange={qrCodeEncoder}
+            placeholder="https://hackernoon.com"
+          />
+                   {" "}
+          <button type="submit" disabled={!url}>
+                        Download QR code          {" "}
+          </button>
+                 {" "}
+        </form>
+             {" "}
+      </div>
+         {" "}
+    </div>
+  );
+};
+
+export default QrCode;
